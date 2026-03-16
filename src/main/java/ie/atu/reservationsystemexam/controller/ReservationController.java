c465bbf9f68f4245e15b935a938c197b2747eab4
limzhilit <g00440629@atu.ie>
2026-03-16 12:50:15 +0000

    Enhance reservation system with additional query endpoints and exception handling improvements

--- src/main/java/ie/atu/reservationsystemexam/controller/ReservationController.java
+++ src/main/java/ie/atu/reservationsystemexam/controller/ReservationController.java
@@ -4,11 +4,15 @@ import ie.atu.reservationsystemexam.model.Reservation;
 import ie.atu.reservationsystemexam.service.ReservationService;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
+import org.springframework.cglib.core.Local;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
+
 
 @RestController
 @RequestMapping("/reservations")
@@ -27,4 +31,31 @@ public class ReservationController {
   public ResponseEntity<List<Reservation>> getAll() {
     return ResponseEntity.ok(reservationService.getAllReservations());
   }
+
+  @GetMapping("/{id}")
+  public ResponseEntity<Reservation> getById(@PathVariable Long id) {
+    return ResponseEntity.ok(reservationService.getReservationById(id));
+  }
+
+  @GetMapping("/date/{date}")
+  public ResponseEntity<List<Reservation>> getByDate(@PathVariable LocalDate date) {
+    return ResponseEntity.ok(reservationService.getReservationsByDate(date));
+  }
+
+  @GetMapping("/dateAndTag/{date}/{tag}")
+  public ResponseEntity<List<Reservation>> getByDate(
+      @PathVariable LocalDate date,
+      @PathVariable String tag
+  ) {
+    return ResponseEntity.ok(reservationService.getReservationsByTagAndDate(tag, date));
+  }
+
+  @GetMapping("/email/{email}")
+  public ResponseEntity<List<Reservation>> getByEmail(@PathVariable String email) {
+    return ResponseEntity.ok(reservationService.getReservationsByEmail(email));
+  }
+
+
+
+
 }
